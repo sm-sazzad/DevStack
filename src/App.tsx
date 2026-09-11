8aeea9166b940718372f3013fb03e30a37387c7c
sm-sazzad <sazzadhossain5758@gmail.com>
2026-09-11 08:32:40 +0600

    Create a vite project and json data file

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HeroSection from './Components/HeroSection'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  )
+}
+
+export default App
